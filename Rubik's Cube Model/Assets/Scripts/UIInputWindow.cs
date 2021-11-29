768fd13008adf6d4b4ef7d6548419c599d9f2740
carlsonr2835 <71044907+carlsonr2835@users.noreply.github.com>
2021-11-29 12:09:21 -0800

    side movement progress

--- Rubik's Cube Model/Assets/Scripts/UIInputWindow.cs
+++ Rubik's Cube Model/Assets/Scripts/UIInputWindow.cs
@@ -5,8 +5,8 @@ using UnityEngine;
 public class UIInputWindow : MonoBehaviour
 {
     private string input;
-    RotateBigCube rotateBigCube;    PivotRotation pivotRotation;    CubeState cubeState;    ReadCube readCube;    // Start is called before the first frame update    void Start()
-    {        pivotRotation = FindObjectOfType<PivotRotation>();    }
+    RotateBigCube rotateBigCube;    //PivotRotation pivotRotation;    CubeState cubeState;    ReadCube readCube;    // Start is called before the first frame update    void Start()
+    {       // pivotRotation = FindObjectOfType<PivotRotation>();    }
 
     // Update is called once per frame
     void Update()
@@ -15,100 +15,129 @@ public class UIInputWindow : MonoBehaviour
     public void ReadStringInput(string s)
     {
         input = s;
-        rotateBigCube = GameObject.Find("CubeHolder").GetComponent<RotateBigCube>();        readCube.ReadState();        List<GameObject> side = new List<GameObject>()        {            cubeState.up,            cubeState.down,            cubeState.right,            cubeState.left,            cubeState.back,            cubeState.front        };                //interpreting input        //single layer turns        if (input == "U")        {
+        rotateBigCube = GameObject.Find("CubeHolder").GetComponent<RotateBigCube>();        //readCube.ReadState();        /*List<GameObject> side = new List<GameObject>()        {            cubeState.up,            cubeState.down,            cubeState.right,            cubeState.left,            cubeState.back,            cubeState.front        };*/                //interpreting input        //single layer turns        if (input == "U")        {
             Debug.Log("Turn up clockwise");
-            pivotRotation.SpinSide(cubeState.up);
+            rotateBigCube.UClock();
         }
         else if (input == "U'")        {
             Debug.Log("Turn up counterclockwise");
+            rotateBigCube.UCounter();
         }
         else if (input == "D")        {
             Debug.Log("Turn down clockwise");
+            rotateBigCube.DClock();
         }
         else if (input == "D'")        {
             Debug.Log("Turn down counterclockwise");
+            rotateBigCube.DCounter();
         }
         else if (input == "B")        {
             Debug.Log("Turn back clockwise");
+            rotateBigCube.BClock();
         }
         else if (input == "B'")        {
             Debug.Log("Turn back counterclockwise");
+            rotateBigCube.BCounter();
         }
         else if (input == "F")        {
             Debug.Log("Turn front clockwise");
+            rotateBigCube.FClock();
         }
         else if (input == "F'")        {
             Debug.Log("Turn front counterclockwise");
+            rotateBigCube.FCounter();
         }
         else if (input == "L")        {
             Debug.Log("Turn left clockwise");
+            rotateBigCube.LClock();
         }
         else if (input == "L'")        {
             Debug.Log("Turn left counterclockwise");
+            rotateBigCube.LCounter();
         }
         else if (input == "R")        {
             Debug.Log("Turn right clockwise");
+            rotateBigCube.RClock();
         }
         else if (input == "R'")        {
             Debug.Log("Turn right counterclockwise");
+            rotateBigCube.RCounter();
         }
 
         //double layer turns
         else if (input == "u")        {
             Debug.Log("Turn 2 up clockwise");
+            rotateBigCube.uClock();
         }
         else if (input == "u'")        {
             Debug.Log("Turn 2 up counterclockwise");
+            rotateBigCube.uCounter();
         }
         else if (input == "d")        {
             Debug.Log("Turn 2 down clockwise");
+            rotateBigCube.dClock();
         }
         else if (input == "d'")        {
             Debug.Log("Turn 2 down counterclockwise");
+            rotateBigCube.dCounter();
         }
         else if (input == "b")        {
             Debug.Log("Turn 2 back clockwise");
+            rotateBigCube.bClock();
         }
         else if (input == "b'")        {
             Debug.Log("Turn 2 back counterclockwise");
+            rotateBigCube.bCounter();
         }
         else if (input == "f")        {
             Debug.Log("Turn 2 front clockwise");
+            rotateBigCube.fClock();
         }
         else if (input == "f'")        {
             Debug.Log("Turn 2 front counterclockwise");
+            rotateBigCube.fCounter();
         }
         else if (input == "l")        {
             Debug.Log("Turn 2 left clockwise");
+            rotateBigCube.lClock();
         }
         else if (input == "l'")        {
             Debug.Log("Turn 2 left counterclockwise");
+            rotateBigCube.lCounter();
         }
         else if (input == "r")        {
             Debug.Log("Turn 2 right clockwise");
+            rotateBigCube.rClock();
         }
         else if (input == "r'")        {
             Debug.Log("Turn 2 right counterclockwise");
+            rotateBigCube.rCounter();
         }
 
         //middle layer turns
         else if (input == "M")        {
             Debug.Log("Turn middle clockwise");
+            rotateBigCube.MClock();
         }
         else if (input == "M'")        {
             Debug.Log("Turn middle counterclockwise");
+            rotateBigCube.MCounter();
         }
         else if (input == "E")        {
             Debug.Log("Turn equator clockwise");
+            rotateBigCube.EClock();
         }
         else if (input == "E'")        {
             Debug.Log("Turn equator counterclockwise");
+            rotateBigCube.ECounter();
         }
         else if (input == "S")        {
             Debug.Log("Turn side clockwise");
+            rotateBigCube.SClock();
         }
         else if (input == "S'")        {
             Debug.Log("Turn side counterclockwise");
+            rotateBigCube.SCounter();
         }
 
         //rotations
