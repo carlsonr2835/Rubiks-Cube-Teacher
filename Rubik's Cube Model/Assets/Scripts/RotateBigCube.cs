1ba1d06ccd432b018849f2d882f2c088c59cd264
carlsonr2835 <71044907+carlsonr2835@users.noreply.github.com>
2021-11-30 12:47:23 -0800

    woking on movement with lists

--- Rubik's Cube Model/Assets/Scripts/RotateBigCube.cs
+++ Rubik's Cube Model/Assets/Scripts/RotateBigCube.cs
@@ -6,33 +6,33 @@ public class RotateBigCube : MonoBehaviour
 {
     public GameObject Cube;
 
-    public GameObject BD;
-    public GameObject LBD;
-    public GameObject RBD;
-    public GameObject D;
-    public GameObject LD;
-    public GameObject RD;
-    public GameObject FD;
-    public GameObject FLD;
-    public GameObject FRD;
-    public GameObject B;
-    public GameObject LB;
+    public GameObject YO;
+    public GameObject YOG;
+    public GameObject YOB;
+    public GameObject Down;
+    public GameObject YG;
+    public GameObject YB;
+    public GameObject YR;
+    public GameObject YRG;
+    public GameObject YRB;
+    public GameObject Back;
+    public GameObject OG;
+    public GameObject OB;
+    public GameObject Right;
+    public GameObject Left;
+    public GameObject Middle;
     public GameObject RB;
-    public GameObject R;
-    public GameObject L;
-    public GameObject M;
-    public GameObject FR;
-    public GameObject FL;
-    public GameObject F;
-    public GameObject U;
-    public GameObject UB;
-    public GameObject UBL;
-    public GameObject UBR;
-    public GameObject UR;
-    public GameObject UL;
-    public GameObject UFR;
-    public GameObject UFL;
-    public GameObject UF;
+    public GameObject RG;
+    public GameObject Front;
+    public GameObject Up;
+    public GameObject WO;
+    public GameObject WOG;
+    public GameObject WOB;
+    public GameObject WB;
+    public GameObject WG;
+    public GameObject WRB;
+    public GameObject WRG;
+    public GameObject WR;
 
     private float clockAngle = 90.0f;
     private float counterAngle = -90.0f;
@@ -40,7 +40,8 @@ public class RotateBigCube : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-
+        List<List<List<GameObject>>> pieces = new List<List<List<GameObject>>>();
+        for (int x = 0; x < 3; x++)        {            List<List<GameObject>> twoDim = new List<List<GameObject>>();            for (int y = 0; y < 3; y++)            {                List<GameObject> oneDim = new List<GameObject>();                for (int z = 0; z < 3; z++)                {                    oneDim.Add(null);                }                twoDim.Add(oneDim);            }            pieces.Add(twoDim);        }
     }
 
     // Update is called once per frame
@@ -75,12 +76,12 @@ public class RotateBigCube : MonoBehaviour
         Cube.transform.Rotate(0, 0, -90, Space.World);
     }
     //side rotations
-    public void UClock()    {        UB.transform.parent = U.transform;        UBL.transform.parent = U.transform;        UBR.transform.parent = U.transform;        UR.transform.parent = U.transform;        UL.transform.parent = U.transform;        UFR.transform.parent = U.transform;        UFL.transform.parent = U.transform;        UF.transform.parent = U.transform;        U.transform.Rotate(0, 90, 0, Space.World);        UB.transform.parent = Cube.transform;        UBL.transform.parent = Cube.transform;        UBR.transform.parent = Cube.transform;        UR.transform.parent = Cube.transform;        UL.transform.parent = Cube.transform;        UFR.transform.parent = Cube.transform;        UFL.transform.parent = Cube.transform;        UF.transform.parent = Cube.transform;    }
-    public void UCounter()    {        UB.transform.parent = U.transform;        UBL.transform.parent = U.transform;        UBR.transform.parent = U.transform;        UR.transform.parent = U.transform;        UL.transform.parent = U.transform;        UFR.transform.parent = U.transform;        UFL.transform.parent = U.transform;        UF.transform.parent = U.transform;        U.transform.Rotate(0, -90, 0, Space.World);        UB.transform.parent = Cube.transform;        UBL.transform.parent = Cube.transform;        UBR.transform.parent = Cube.transform;        UR.transform.parent = Cube.transform;        UL.transform.parent = Cube.transform;        UFR.transform.parent = Cube.transform;        UFL.transform.parent = Cube.transform;        UF.transform.parent = Cube.transform;    }
-    public void DClock()    {        BD.transform.parent = D.transform;        LBD.transform.parent = D.transform;        RBD.transform.parent = D.transform;        LD.transform.parent = D.transform;        RD.transform.parent = D.transform;        FD.transform.parent = D.transform;        FLD.transform.parent = D.transform;        FRD.transform.parent = D.transform;        D.transform.Rotate(0, -90, 0, Space.World);        BD.transform.parent = Cube.transform;        LBD.transform.parent = Cube.transform;        RBD.transform.parent = Cube.transform;        LD.transform.parent = Cube.transform;        RD.transform.parent = Cube.transform;        FD.transform.parent = Cube.transform;        FLD.transform.parent = Cube.transform;        FRD.transform.parent = Cube.transform;    }
-    public void DCounter()    {        BD.transform.parent = D.transform;        LBD.transform.parent = D.transform;        RBD.transform.parent = D.transform;        LD.transform.parent = D.transform;        RD.transform.parent = D.transform;        FD.transform.parent = D.transform;        FLD.transform.parent = D.transform;        FRD.transform.parent = D.transform;        D.transform.Rotate(0, 90, 0, Space.World);        BD.transform.parent = Cube.transform;        LBD.transform.parent = Cube.transform;        RBD.transform.parent = Cube.transform;        LD.transform.parent = Cube.transform;        RD.transform.parent = Cube.transform;        FD.transform.parent = Cube.transform;        FLD.transform.parent = Cube.transform;        FRD.transform.parent = Cube.transform;    }
-    public void BClock()    {        BD.transform.parent = B.transform;        LBD.transform.parent = B.transform;        RBD.transform.parent = B.transform;        LB.transform.parent = B.transform;        RB.transform.parent = B.transform;        UB.transform.parent = B.transform;        UBL.transform.parent = B.transform;        UBR.transform.parent = B.transform;        B.transform.Rotate(90, 0, 0, Space.World);        BD.transform.parent = Cube.transform;        LBD.transform.parent = Cube.transform;        RBD.transform.parent = Cube.transform;        LB.transform.parent = Cube.transform;        RB.transform.parent = Cube.transform;        UB.transform.parent = Cube.transform;        UBL.transform.parent = Cube.transform;        UBR.transform.parent = Cube.transform;    }
-    public void BCounter()    {        BD.transform.parent = B.transform;        LBD.transform.parent = B.transform;        RBD.transform.parent = B.transform;        LB.transform.parent = B.transform;        RB.transform.parent = B.transform;        UB.transform.parent = B.transform;        UBL.transform.parent = B.transform;        UBR.transform.parent = B.transform;        B.transform.Rotate(-90, 0, 0, Space.World);        BD.transform.parent = Cube.transform;        LBD.transform.parent = Cube.transform;        RBD.transform.parent = Cube.transform;        LB.transform.parent = Cube.transform;        RB.transform.parent = Cube.transform;        UB.transform.parent = Cube.transform;        UBL.transform.parent = Cube.transform;        UBR.transform.parent = Cube.transform;    }
+    public void UClock()    {        /*UB.transform.parent = U.transform;        UBL.transform.parent = U.transform;        UBR.transform.parent = U.transform;        UR.transform.parent = U.transform;        UL.transform.parent = U.transform;        UFR.transform.parent = U.transform;        UFL.transform.parent = U.transform;        UF.transform.parent = U.transform;        U.transform.Rotate(0, 90, 0, Space.World);        UB.transform.parent = Cube.transform;        UBL.transform.parent = Cube.transform;        UBR.transform.parent = Cube.transform;        UR.transform.parent = Cube.transform;        UL.transform.parent = Cube.transform;        UFR.transform.parent = Cube.transform;        UFL.transform.parent = Cube.transform;        UF.transform.parent = Cube.transform;*/    }
+    public void UCounter()    {        /*UB.transform.parent = U.transform;        UBL.transform.parent = U.transform;        UBR.transform.parent = U.transform;        UR.transform.parent = U.transform;        UL.transform.parent = U.transform;        UFR.transform.parent = U.transform;        UFL.transform.parent = U.transform;        UF.transform.parent = U.transform;        U.transform.Rotate(0, -90, 0, Space.World);        UB.transform.parent = Cube.transform;        UBL.transform.parent = Cube.transform;        UBR.transform.parent = Cube.transform;        UR.transform.parent = Cube.transform;        UL.transform.parent = Cube.transform;        UFR.transform.parent = Cube.transform;        UFL.transform.parent = Cube.transform;        UF.transform.parent = Cube.transform;*/    }
+    public void DClock()    {        /*BD.transform.parent = D.transform;        LBD.transform.parent = D.transform;        RBD.transform.parent = D.transform;        LD.transform.parent = D.transform;        RD.transform.parent = D.transform;        FD.transform.parent = D.transform;        FLD.transform.parent = D.transform;        FRD.transform.parent = D.transform;        D.transform.Rotate(0, -90, 0, Space.World);        BD.transform.parent = Cube.transform;        LBD.transform.parent = Cube.transform;        RBD.transform.parent = Cube.transform;        LD.transform.parent = Cube.transform;        RD.transform.parent = Cube.transform;        FD.transform.parent = Cube.transform;        FLD.transform.parent = Cube.transform;        FRD.transform.parent = Cube.transform;*/    }
+    public void DCounter()    {        /*BD.transform.parent = D.transform;        LBD.transform.parent = D.transform;        RBD.transform.parent = D.transform;        LD.transform.parent = D.transform;        RD.transform.parent = D.transform;        FD.transform.parent = D.transform;        FLD.transform.parent = D.transform;        FRD.transform.parent = D.transform;        D.transform.Rotate(0, 90, 0, Space.World);        BD.transform.parent = Cube.transform;        LBD.transform.parent = Cube.transform;        RBD.transform.parent = Cube.transform;        LD.transform.parent = Cube.transform;        RD.transform.parent = Cube.transform;        FD.transform.parent = Cube.transform;        FLD.transform.parent = Cube.transform;        FRD.transform.parent = Cube.transform;*/    }
+    public void BClock()    {        /*BD.transform.parent = B.transform;        LBD.transform.parent = B.transform;        RBD.transform.parent = B.transform;        LB.transform.parent = B.transform;        RB.transform.parent = B.transform;        UB.transform.parent = B.transform;        UBL.transform.parent = B.transform;        UBR.transform.parent = B.transform;        B.transform.Rotate(90, 0, 0, Space.World);        BD.transform.parent = Cube.transform;        LBD.transform.parent = Cube.transform;        RBD.transform.parent = Cube.transform;        LB.transform.parent = Cube.transform;        RB.transform.parent = Cube.transform;        UB.transform.parent = Cube.transform;        UBL.transform.parent = Cube.transform;        UBR.transform.parent = Cube.transform;*/    }
+    public void BCounter()    {        /*BD.transform.parent = B.transform;        LBD.transform.parent = B.transform;        RBD.transform.parent = B.transform;        LB.transform.parent = B.transform;        RB.transform.parent = B.transform;        UB.transform.parent = B.transform;        UBL.transform.parent = B.transform;        UBR.transform.parent = B.transform;        B.transform.Rotate(-90, 0, 0, Space.World);        BD.transform.parent = Cube.transform;        LBD.transform.parent = Cube.transform;        RBD.transform.parent = Cube.transform;        LB.transform.parent = Cube.transform;        RB.transform.parent = Cube.transform;        UB.transform.parent = Cube.transform;        UBL.transform.parent = Cube.transform;        UBR.transform.parent = Cube.transform;*/    }
     public void FClock()    {    }
     public void FCounter()    {    }
     public void LClock()    {    }
